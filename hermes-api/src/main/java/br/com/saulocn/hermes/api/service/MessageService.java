b8d04b6eb485445cf32c71403f183ce6ea857bdc
Saulo de Carvalho Neto <saulocn@gmail.com>
2022-06-09 23:33:31 -0300

    Adicionando o cache e cadastrando na inserção da mensagem

--- hermes-api/src/main/java/br/com/saulocn/hermes/api/service/MessageService.java
+++ hermes-api/src/main/java/br/com/saulocn/hermes/api/service/MessageService.java
@@ -3,13 +3,17 @@ package br.com.saulocn.hermes.api.service;
 import br.com.saulocn.hermes.api.entity.Message;
 import br.com.saulocn.hermes.api.entity.Recipient;
 import br.com.saulocn.hermes.api.resource.request.MessageVO;
+import br.com.saulocn.hermes.api.service.vo.MailVO;
+import io.quarkus.redis.client.RedisClient;
+import io.vertx.redis.client.Response;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class MessageService {
@@ -17,15 +21,34 @@ public class MessageService {
     @Inject
     EntityManager em;
 
+    @Inject
+    RedisClient redisClient;
+
+
+    @Inject
+    Logger log;
+
+
     @Transactional
     public Message sendMail(MessageVO messageVO){
         Message message = Message.of(messageVO);
         em.persist(message);
         messageVO.getRecipients().stream().forEach(recipient -> em.persist(new Recipient(recipient, message.getId())));
+        redisClient.set(Arrays.asList(getMessageKey(message.getId()), MailVO.of(messageVO).toJSON()));
         return message;
+    }
+
+    private String getMessageKey(Long messageId) {
+        return String.format("message_%d",messageId);
     }
 
     public List<Message> listMail() {
         return em.createQuery("select m from Message m", Message.class).getResultList();
+    }
+
+    public MailVO findById(Long messageId) {
+        String mailVOJSON = redisClient.get(getMessageKey(messageId)).toString();
+        log.info("Found: "+mailVOJSON);
+        return MailVO.fromJSON(mailVOJSON);
     }
 }
---
+++ hermes-api/src/main/java/br/com/saulocn/hermes/api/service/vo/MailVO.java
@@ -0,0 +1,67 @@
+package br.com.saulocn.hermes.api.service.vo;
+
+import br.com.saulocn.hermes.api.resource.request.MessageVO;
+
+import javax.json.bind.JsonbBuilder;
+import java.util.Objects;
+
+public class MailVO {
+
+    private Long messageId;
+    private String subject;
+    private String text;
+    private String contentType;
+
+    public MailVO() {
+    }
+
+    public static MailVO of(MessageVO messageVO) {
+        MailVO mailVO = new MailVO();
+        mailVO.setMessageId(messageVO.getId());
+        mailVO.setText(messageVO.getText());
+        mailVO.setSubject(messageVO.getTitle());
+        mailVO.setContentType(messageVO.getContentType());
+        return mailVO;
+    }
+
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String toJSON() {
+        return JsonbBuilder.create().toJson(this);
+    }
+
+    public static MailVO fromJSON(String json) {
+        return JsonbBuilder.create().fromJson(json, MailVO.class);
+    }
+}
